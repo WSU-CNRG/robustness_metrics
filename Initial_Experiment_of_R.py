09757de85086745d91364c87da6fae763c10cdb1
BalumeMburano <45223634+BalumeMburano@users.noreply.github.com>
2019-05-01 12:23:59 +1000

    Create Initial_Experiment_of_R.py
    The above returns the a graph and its R value

---
+++ Initial_Experiment_of_R.py
@@ -0,0 +1,43 @@
+"""
+@author: Balume
+"""
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import R_metric
+
+try:
+    reload
+    
+except NameError:
+    try:
+        from importlib import reload  
+    except ImportError:
+        from imp import reload 
+
+def display_graph(G):
+    nx.draw(G)
+    plt.show() 
+    
+def main():
+    try:
+       
+        graphs=input('Enter the number of graphs : ')
+        i=0
+        while i in range(int(graphs)):
+            ''' reload R_metric'''
+            reload(R_metric)
+            '''Create graph'''
+            #G=nx.gnm_random_graph(100,400)
+            #G=nx.random_regular_graph(5,20,seed=None)
+            G=nx.barabasi_albert_graph(100,50)
+            '''display graphs'''            
+            display_graph(G)
+            '''call  class that return the mertic R with node degree as the node importance measure''' 
+            R_metric.metric_r_degree(G)
+            i+=1 
+       
+    except Exception as e:
+        print(str(e))
+        pass
+main()
